f0ca315832c0b4602132574dbfec37f34da0ff0f
gff1055 <guilhermeferreirafb@gmail.com>
2020-01-27 15:05:44 -0300

    Implementacao de recorde

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-//VARIAVEIS DO JOGO
+//Variaveis do jogo
 
-var canvas, ctx,    // VARIAVEIS USADAS PARA DESENHAR OS ITENS
+var canvas, ctx,    // Variaveis usadas para desenhar os itens
 
-// VARIAVEIS USADAS PARA PEGAR O TAMANHO DA JANELA DO NAVEGADOR
+// Variaveis usadas para pegar o tamanho da janela do navegador
 altura = 0, 
 largura = 0,
 
@@ -11,7 +11,7 @@ frames = 0, // VARIAVEL USADA NO CONTROLE DA ANIMAÇÃO DO JOGO
 maxPulos = 3,   // QUANTIDADE MAXIMA DE PULOS
 velocidade = 10,    // VARIAVEL PARA A VELOCIDADE DOS OBSTACULOS
 estadoAtual,    // Variavel que gerencia o estado atual do jogo
-record,
+record, // Variavel que armazena os recordes
 
 // Variavel recebe um array que contem os possiveis estados do jogo
 estados =   {
@@ -30,11 +30,10 @@ chao =  {
 
             
             // FUNCAO PARA DESENHAR O CHAO
-            desenha:    function()
-                        {
+            desenha:    function(){
                             ctx.fillStyle = this.cor;   // MUDANDO A COR DO CONTEXTO NO CHAO
                             ctx.fillRect(0, this.y, largura, this.altura);  // DESENHANDO O RETANGULO QUE REPRESENTA O CHAO
-                        },
+            },
 },
 
 
@@ -49,20 +48,18 @@ bloco =     {
                 velocidade: 0,  // VELOCIDADE DE QUEDA DO BLOCO
                 forcaDoPulo: 23.6,  // FORÇA DO PULO DO BLOCO
                 qntPulos: 0,    // QUANTIDADE DE PULOS QUE O BLOCO FEZ
-                score: 0,
+                score: 0,   // Variavel para contagem do placar do jogador
 
 
                 // ATUALIZA A VELOCIDADE E A COORDENADA y DO BLOCO (QUEDA)
-                atualiza:   function()
-                            {
+                atualiza:   function(){
                                 this.velocidade += this.gravidade;  // INCREMENTANDO A VELOCIDADE DE QUEDA COM O VALOR DA GRAVIDADE
                                 this.y += this.velocidade;  // MUDANDO O y COM O VALOR DA VELOCIDADE DE QUEDA
                                 
                                 /* MANTENDO O BLOCO NO CHAO */
                                 // Condicional... 
                                 if(this.y > chao.y - this.altura    // ... caso o bloco chegue ao chao
-                                    && estadoAtual!=estados.perdeu) // ... e caso o usuario nao tenha perdido o jogo
-                                {
+                                && estadoAtual!=estados.perdeu){ // ... e caso o usuario nao tenha perdido o jogo
                                     this.y = chao.y - this.altura;  // FORÇANDO O BLOCO A 'FICAR' EM CIMA DO CHAO
                                     this.qntPulos = 0;  // RESETANDO A COTA MAXIMA DE PULOS DO BLOCO
                                     this.velocidade = 0;    // Reseta a velocidade do bloco apos tocar o chao
@@ -71,35 +68,31 @@ bloco =     {
 
 
                 // METODO PARA FAZER O BLOCO PULAR
-                pula:   function()
-                        {
+                pula:   function(){
                             // VERIFICANDO SE O BLOCO JA ATINGIU A COTA MAXIMA DE PULOS
-                            if(this.qntPulos < maxPulos)
-                            {
+                            if(this.qntPulos < maxPulos){
                                 this.velocidade = -this.forcaDoPulo;    // MUDANDO A VELOCIDADE DO BLOCO (RELATIVO A FORÇA DO PULO)
                                 this.qntPulos++;    // AUMENTANDO A QUANTIDADE CADA VEZ QUE O BLOCO PULA
                             }
                 },
 
-
-                reset: function()
-                {
+                // Metodo para resetar os atributos (y, velocidade, score) do bloco
+                reset: function(){
                     this.y = 0;    // O bloco é colocado em cima
                     this.velocidade = 0;   // reseta a velocidade do bloco apos o clique inicial
 
-                    if(this.score > record)
-                    {
-                        localStorage.setItem("record", this.score);
-                        record = this.score;
+                    // Caso o usuario tenha batido o record
+                    if(this.score > record){
+                        localStorage.setItem("record", this.score); // Guardando no LocalStorage o novo recorde
+                        record = this.score;    // variavel 'record' recebe a nova pontuação
                     }
 
-                    this.score = 0;
+                    this.score = 0; // O placar é zerado para o reinicio do jogo
                 },
 
 
                 // METODO PARA DESENHAR O BLOCO
-                desenha:    function()
-                            {
+                desenha:    function(){
                                 ctx.fillStyle = this.cor;   // MUDANDO A COR DO CONTEXTO DO BLOCO
                                 ctx.fillRect(this.x, this.y, this.largura, this.altura);    // DESENHANDO O RETANGULO QUE REPRESENTA O BLOCO
                 }
@@ -114,14 +107,11 @@ obstaculos =    {
                     tempoInsere:0,  // Variavel que ajuda no tempo inserção dos obstaculos
 
                     // METODO PARA INSERIR UM ELEMENTO NO ARRAY DE OBSTACULOS
-                    insere: function()
-                            {
-
+                    insere: function(){
                                 // INSERINDO O OBSTACULO NO ARRAY
                                 this._obs.push( {
                                                     x: largura, // POSICAO X INICIAL
-                                                    //largura: 30 + Math.floor(21 * Math.random()),   // LARGURA DO OBSTACULO
-                                                    largura: 50,
+                                                    largura: 50,    // LARGURA DO OBSTACULO
                                                     altura: 30 + Math.floor(120 * Math.random()),   // ALTURA DO OBSTACULO
                                                     cor: this.cores[Math.floor(5 * Math.random())]  // COR DO OBSTACULO
                                 });
@@ -130,34 +120,29 @@ obstaculos =    {
                     },
 
                     // METODO PARA ATUALIZAR O ARRAY DE OBSTACULOS
-                    atualiza:   function()
-                                {
+                    atualiza:   function(){
                                     // Se o temporizador chegar a zero
-                                    if(this.tempoInsere == 0)
-                                        this.insere();  // Inserindo um bloco
+                                    if(this.tempoInsere == 0) this.insere();  // Inserindo um bloco
                                     
                                     // O temporizador ainda não é zero
-                                    else 
-                                        this.tempoInsere--; // Decrementando o temporizador
+                                    else this.tempoInsere--; // Decrementando o temporizador
 
                                     // Rodando o array de obstaculos
-                                    for(var i = 0, tam = this._obs.length; i < tam; i++)
-                                    {
+                                    for(var i = 0, tam = this._obs.length; i < tam; i++){
                                         var obs = this._obs[i]; // SELECIONANDO O ELEMENTO(OBSTACULO) ATUAL
                                         obs.x -= velocidade;    // DECREMENTANDO O VALOR DE X EM RELAÇÃO A VELOCIDADE DO OBSTACULO
 
-                                        // Condicional nos casos onde haver colisao do bloco com obstaculos
+                                        // Houve colisao do bloco com um obstaculo?
                                         if(bloco.x < obs.x + obs.largura &&
                                             bloco.x + bloco.largura >= obs.x &&
                                             bloco.y + bloco.altura >= chao.y - obs.largura)
                                                 estadoAtual = estados.perdeu;   // O usuario perde o jogo
 
-                                        else if(obs.x == 0)
-                                            bloco.score++;
+                                        // O bloco pulou o obstaculo?
+                                        else if(obs.x == 0) bloco.score++;  // O usuario marcou um ponto
                                         
-                                        /** Condicional quando o obstaculo sair da area de visao **/
-                                        else if(obs.x <= -obs.largura)
-                                        {
+                                        /** O obstaculo saiu da area de visao na tela? **/
+                                        else if(obs.x <= -obs.largura){
                                             this._obs.splice(i,1);  // Removendo o obstaculo do array
                                             i--;    // Atualizando o indice atual após a esclusão
                                             tam--;  // Atualizando o tamanho do array após a esclusão
@@ -166,115 +151,121 @@ obstaculos =    {
                     },
 
                     // Metodo para limpar o array de obstaculos apos o usuario perder uma partida
-                    limpa:  function()
-                            {
+                    limpa:  function(){
                                 this._obs = []; // Limpando o array de obstaculos
                     },
 
                     // METODO PARA DESENHAR O OBSTACULO NA TELA
-                    desenha:    function()
-                                {
+                    desenha:    function(){
                                     // PERCORRENDO O ARRAY PARA DESENHAR OS BLOCOS QUE ESTAO NO MESMO
-                                    for(var i = 0, tam = this._obs.length; i < tam; i++)
-                                    {
-                                        var obs = this._obs[i]; // MUDANDO A COR DO CONTEXTO DO BLOCO
-                                        ctx.fillStyle = obs.cor;
+                                    for(var i = 0, tam = this._obs.length; i < tam; i++){
+                                        var obs = this._obs[i]; 
+                                        ctx.fillStyle = obs.cor;    // MUDANDO A COR DO CONTEXTO DO BLOCO
                                         ctx.fillRect(obs.x, chao.y - obs.altura, obs.largura, obs.altura);  // DESENHANDO O BLOCO
-                                        
                                     }
                     }
 };
 
 
 // FUNCAO QUE IDENTIFICA SE HOUVE CLIQUE
-function clique(evt)
-{
-    // Condicional caso o jogo esteja sendo executado
+function clique(evt){
+    // O jogo esteja sendo executado?
     if(estadoAtual == estados.jogando)
         bloco.pula();   // O bloco pula
 
-    // Condicional caso o jogo esteja prestes a ser executado
+    // O jogo esta pronto para ser executado?
     else if(estadoAtual == estados.jogar)
         estadoAtual = estados.jogando;  // Agora o jogo esta sendo executado
 
-    // Condicional caso... 
-    else if(estadoAtual == estados.perdeu   // ... do usuario tenha perdido o jogo
-        && bloco.y >= 2*altura)   // Faz o usuario ter que esperar para jogar novamente
-    {
-        estadoAtual = estados.jogar;    // Agora o jogo esta pronto para ser iniciado
+    // O usuario perdeu o jogo ??
+    else if(estadoAtual == estados.perdeu
+    && bloco.y >= 2*altura){   // Travando o usuario para jogar novamente
         obstaculos.limpa(); // Limpa o array de obstaculos
-        bloco.reset();
+        bloco.reset();  // Resetando os atributo do bloco
+        estadoAtual = estados.jogar;    // Agora o jogo esta pronto para ser iniciado
     }
 }
 
 
 
 // FUNCAO PARA ATUALIZAR O STATUS DO PERSONAGEM E DOS BLOCOS
-function atualiza()
-{
+function atualiza(){
     frames++;   // INCREMENTANDO O FRAMES DO JOGO
     bloco.atualiza();   // ATUALIZANDO A VELOCIDADE E COORDENADA y DO BLOCO (QUEDA ou CLIQUE)
 
-    // Condicional no caso do jogo estar executando
-    if(estadoAtual == estados.jogando) obstaculos.atualiza();  // ATUALIZANDO O ESTADO DOS OBSTACULOS (VELOCIDADE)
+    // O jogo está executando
+    if(estadoAtual == estados.jogando)
+        obstaculos.atualiza();  // ATUALIZANDO O ESTADO DOS OBSTACULOS (VELOCIDADE)
 }
 
 
 // FUNCAO USADA PARA DESENHAR (PERSONAGEM, BLOCOS, CHAO, ETC...) DEPOIS DE ATUALIZAR
-function desenha()
-{
+function desenha(){
+
+    //Desenhando o fundo do jogo
     ctx.fillStyle = "#80daff";  // PINTANDO O FUNDO DA CANVAS DE AZUL
     ctx.fillRect(0, 0, largura, altura);    // TAMANHO DA AREA DE PINTURA
 
-    ctx.fillStyle = "#fff";
-    ctx.font = "50px Arial";
-    ctx.fillText(bloco.score, 30, 68);
+    // Desenhando o score
+    ctx.fillStyle = "#fff"; // Cor do placar
+    ctx.font = "50px Arial";    // Fonte do placar
+    ctx.fillText(bloco.score, 30, 68);  // Desenhando o placar na tela
     
-
-    // Condicional nos casos do jogo estar esecutando
-    if(estadoAtual == estados.jogar)
-    {
+    // O jogo está pronto para rodar?
+    if(estadoAtual == estados.jogar){
         ctx.fillStyle = "green";    // Preenche o contexto de verde
         ctx.fillRect(largura/2 - 50, altura/2 - 50, 100, 100);  // Desenha o quadrado
     }
 
-    // Condicional no caso do usuario ter perdido o jogo
-    else if(estadoAtual == estados.perdeu)
-    {
+    // O jogador perdeu o jogo?
+    else if(estadoAtual == estados.perdeu){
         ctx.fillStyle = "red";  // Preenche o contexto de vermelho
         ctx.fillRect(largura/2 - 50, altura/2 - 50, 100, 100);  // Desenha o quadrado
 
-        ctx.save();
-        ctx.translate(largura/2, altura/2);
-        ctx.fillStyle = "#ffffff";
+        /** DESENHANDO O SCORE NO QUADRADO VERMELHO */
+        ctx.save(); // Salvando a imagem
+        ctx.translate(largura/2, altura/2); // Transladando para o meio da canvas
+        ctx.fillStyle = "#ffffff";  // 
 
-        if(bloco.score > record)
-            ctx.fillText("Novo Record", -150, -65);
+        /** EXIBINDO O PLACAR DO JOGADOR */
 
-        else if(record < 10)
-            ctx.fillText("Record " + record, -99, -65);
-
-        else if(record >= 10 && record < 100)
-            ctx.fillText("Record " + record, -112, -65);
-
-        else
-            ctx.fillText("Record " + record, -125, -65);
-
-
+        // O plcar do jogador tem um digito?
         if(bloco.score < 10)
             ctx.fillText(bloco.score, -13, 19);
 
+        // O placar do jogador tem 2 digitos?
         else if(bloco.score >= 10 && bloco.score < 100)
             ctx.fillText(bloco.score, -26, 19)
 
+        // O placar do jogador tem 3 digitos
         else
             ctx.fillText(bloco.score, -39, 19);
 
-        ctx.restore();
+
+        /** EXIBINDO O RECORDE */
+
+        // O jogador bateu o recorde?
+        if(bloco.score > record)
+            ctx.fillText("Novo Record", -150, -65); 
+
+        // O recorde atual tem um digito?
+        else if(record < 10)
+            ctx.fillText("Record " + record, -99, -65);
+
+        // O recorde atual tem 2 digitos?
+        else if(record >= 10 && record < 100)
+            ctx.fillText("Record " + record, -112, -65);
+
+        // O recorde atual tem 3 digitos
+        else
+            ctx.fillText("Record " + record, -125, -65);
+
+        ctx.restore(); // Restaura as configurações
     }
 
     // Condicional no caso do jogo estar em execucao
-    else if(estadoAtual == estados.jogando) obstaculos.desenha();   // DESENHANDO OS OBSTACULOS
+    else if(estadoAtual == estados.jogando)
+        obstaculos.desenha();   // DESENHANDO OS OBSTACULOS
 
     chao.desenha(); // DESENHANDO O CHAO
     bloco.desenha();    // DESENHANDO O BLOCO
@@ -282,25 +273,20 @@ function desenha()
 
 
 // FUNCAO PARA RODAR O JOGO. ONDE VAI FICAR O LOOP, DESENHADO NA CANVAS
-function roda()
-{
+function roda(){
     atualiza(); // ATUALIZANDO O STATUS DO PERSONAGEM E BLOCOS
     desenha();  // DESENHANDO PERSONAGEM, BLOCOS, CHAO, ETC...
     window.requestAnimationFrame(roda); // CHAMANDO A FUNÇÃO RODA DIRETO (LOOP)
 }
 
 
-
-
 // FUNÇÃO PRINCIPAL DO JOGO
-function main()
-{
+function main(){
     altura = window.innerHeight;    // RECEBENDO A ALTURA DA JANELA DO USUARIO
     largura = window.innerWidth;    // RECEBENDO A LARGURA DA JANELA DO USUARIO
 
     // VERIFICANDO O TAMANHO DA JANELA DO USUARIO
-    if(largura>=500)
-    {        
+    if(largura>=500){        
         // FORÇANDO O TAMANHO DA JANELA PARA UM TAMANHO FIXO
         largura = 600;
         altura = 600;
@@ -313,11 +299,12 @@ function main()
     ctx = canvas.getContext("2d");  // TUDO O QUE FOR DESENHADO SERÁ 2D
     document.body.appendChild(canvas);  // ADICIONANDO A VARIAVEL CANVAS NO CORPO DO HTML
     document.addEventListener("mousedown", clique); // CONFIGURANDO O EVENTO "clique"
-    estadoAtual = estados.jogar;    // Possibilita a exibir primeira tela com o quadrado verde
-    record = localStorage.getItem("record");
+    estadoAtual = estados.jogar;    // O jogo esta pronto para ser iniciado (tela de inicio)
+    record = localStorage.getItem("record");    // Acessando LOCALSTORAGE localStorage procurando pelo item RECORD e o atribui a variavel
 
+    // Houve algum registro de recorde anterior?
     if(record == null)
-        record = 0;
+        record = 0; // Resetando o recorde
 
     roda(); // RODANDO O JOGO
 }
